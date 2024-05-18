157773687a2e473df89794186eb0c959cdd722de
= <antoniochavezmarco@gmail.com>
2024-05-18 09:09:47 -0600

    I guess this is all I can do

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-
+const { Storage } = require('@google-cloud/storage');
+const Multer = require('multer');
+const storage = new Storage();
+const credential = require('./key.json');
+const bucketName = 'storagegrades';
+const bucket = storage.bucket(bucketName);
 //Jala datos del .env
+
+// Autentificarse con Cloud Storage
 require('dotenv').config();
 
 const host = process.env.DB_HOST;
@@ -22,6 +29,13 @@ const connection = mysql.createConnection({
     port: port
 });
 
+multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+    },
+});
+
 connection.connect((error) => {
     if (error) {
         console.error('Error connecting: ' + error.stack);
@@ -32,6 +46,7 @@ connection.connect((error) => {
 
 app.use(cors());
 app.use(bodyParser.json()); // Añadir este middleware para parsear JSON
+
 
 app.post('/', (req, res) => {
     const idUsuario = req.body.idUsuario;
@@ -60,12 +75,13 @@ app.post('/register', (req, res) => {
     const profile = req.body.profile;
     const career = req.body.career;
     const idUsuario = req.body.idUsuario;
+    const aptitudes = req.body.aptitudes;
 
-    if (!profile || !career || !idUsuario) {
+    if (!profile || !career || !idUsuario || !aptitudes) {
         res.status(400).send({ success: false, message: 'Faltan datos requeridos' + JSON.stringify(req.body)});
         return;
     }
-    connection.query('INSERT INTO DataUser (id, profile, career) VALUES (?, ?, ?)', [idUsuario, profile, career], (error, results) => {
+    connection.query('INSERT INTO DataUser (id, profile, career, aptitudes) VALUES (?, ?, ?, ?)', [idUsuario, profile, career], (error, results) => {
         if (error) {
             console.error('Error executing query: ' + error.stack);
             res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de inserción' });
@@ -158,19 +174,15 @@ app.delete('/activitie/:id', (req, res) => {
 
 app.post('/activitie/answer/:id', (req, res) => {
     const answer = req.body.answer;
-    const points = req.body.points;
     const activityId = req.params.id;
 
     if (!answer) {
         res.status(400).send({ success: false, message: 'Falta el campo answer' });
         return;
     }
-    if (!points) {
-        res.status(400).send({ success: false, message: 'Falta el campo points' });
-        return;
-    }
 
-    connection.query('UPDATE Activities SET answerGPT = ?, points = ? WHERE id = ?', [answer, points, activityId], (error, results) => {
+
+    connection.query('UPDATE Activities SET answerGPT = ? WHERE id = ?', [answer, activityId], (error, results) => {
         if (error) {
             console.error('Error executing query: ' + error.stack);
             res.status(500).send({ success: false, message: 'Error al ejecutar la consulta' });
@@ -194,7 +206,6 @@ app.post('/unlock/:id', (req, res) => {
         res.status(400).send({ success: false, message: 'Faltan datos requeridos: idUsuario o points' });
         return;
     }
-
     connection.query('INSERT INTO Avatars (id_user, id_avatar) VALUES (?, ?)', [idUsuario, avatarId], (error, results) => {
         if (error) {
             console.error('Error executing query: ' + error.stack);
@@ -202,7 +213,7 @@ app.post('/unlock/:id', (req, res) => {
             return;
         }
 
-        connection.query('UPDATE DataUser SET points = points - ? WHERE id = ?', [points, idUsuario], (error, results) => {
+        connection.query('UPDATE DataUser SET points = points - ?, avatar_seleccionado = id_avatar WHERE id = ?', [points, idUsuario], (error, results) => {
             if (error) {
                 console.error('Error executing query: ' + error.stack);
                 res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de actualización' });
@@ -219,53 +230,66 @@ app.post('/unlock/:id', (req, res) => {
     });
 });
 
-//Completa la actividad y agrega los puntos al usuario, los puntos a sumar se obtienen de una consulta a la actividad
-app.post('/activitie/complete/:id', (req, res) => {
-    const idUsuario = req.body.idUsuario;
-    const activityId = req.params.id;
+app.post('/changeAvatar/:id', (req, res) => {
+    const { idUsuario } = req.body;
+    const avatarId = req.params.id;
 
     if (!idUsuario) {
         res.status(400).send({ success: false, message: 'Faltan datos requeridos: idUsuario' });
         return;
     }
 
-    connection.query('SELECT points FROM Activities WHERE id = ?', [activityId], (error, results) => {
+    connection.query('UPDATE DataUser SET avatar_seleccionado = ? WHERE id = ?', [avatarId, idUsuario], (error, results) => {
         if (error) {
             console.error('Error executing query: ' + error.stack);
-            res.status(500).send({ success: false, message: 'Error al ejecutar la consulta' });
+            res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de actualización' });
             return;
         }
 
-        if (results.length === 0) {
-            res.status(404).send({ success: false, message: 'Actividad no encontrada' });
+        if (results.affectedRows === 0) {
+            res.status(404).send({ success: false, message: 'Usuario no encontrado' });
             return;
         }
 
-        const points = results[0].points;
-        connection.query('UPDATE Activities SET completed = 1 WHERE id = ?', [activityId], (error, results) => {
-            if (error) {
-                console.error('Error executing query: ' + error.stack);
-                res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de actualización' });
-                return;
-            }
-        }
-        );
-        connection.query('UPDATE DataUser SET points = points + ? WHERE id = ?', [points, idUsuario], (error, results) => {
-            if (error) {
-                console.error('Error executing query: ' + error.stack);
-                res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de actualización' });
-                return;
-            }
-
-            if (results.affectedRows === 0) {
-                res.status(404).send({ success: false, message: 'Usuario no encontrado' });
-                return;
-            }
-
-            res.send({ success: true, message: 'Actividad completada' });
-        });
+        res.send({ success: true, message: 'Avatar cambiado' });
     });
 });
+
+//Completa la actividad y agrega los puntos al usuario, los puntos a sumar se obtienen de una consulta a la actividad
+app.post('/activitie/complete/:id', (req, res) => {
+    const idUsuario = req.body.idUsuario;
+    const evaluacion = req.body.evaluacion;
+    const activityId = req.params.id;
+    const points = req.body.points;
+
+    if (!idUsuario) {
+        res.status(400).send({ success: false, message: 'Faltan datos requeridos: idUsuario' });
+        return;
+    }
+    connection.query('UPDATE Activities SET completed = 1, evaluacion = ? WHERE id = ?', [ evaluacion, activityId], (error, results) => {
+        if (error) {
+            console.error('Error executing query: ' + error.stack);
+            res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de actualización' });
+            return;
+        }
+    }
+    );
+    connection.query('UPDATE DataUser SET points = points + ? WHERE id = ?', [points, idUsuario], (error, results) => {
+        if (error) {
+            console.error('Error executing query: ' + error.stack);
+            res.status(500).send({ success: false, message: 'Error al ejecutar la consulta de actualización' });
+            return;
+        }
+
+        if (results.affectedRows === 0) {
+            res.status(404).send({ success: false, message: 'Usuario no encontrado' });
+            return;
+        }
+
+        res.send({ success: true, message: 'Actividad completada' });
+    });
+});
+
 
 
 app.post('/puntos', (req, res) => {
@@ -315,6 +339,32 @@ app.post('/perfil', (req, res) => {
         res.send({ success: true, data: results[0] });
     });
 });
+
+app.post('/upload', multer.single('file'), (req, res, next) => {
+    if (!req.file) {
+      res.status(400).send('No file uploaded.');
+      return;
+    }
+  
+    // Create a new blob in the bucket and upload the file data.
+    const blob = bucket.file(req.file.originalname);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+  
+    blobStream.on('error', err => {
+      next(err);
+    });
+  
+    blobStream.on('finish', () => {
+      // The public URL can be used to directly access the file via HTTP.
+      const publicUrl = `gs://${bucket.name}/${blob.name}`
+      res.status(200).send({success: true, url: publicUrl});
+    });
+  
+    blobStream.end(req.file.buffer);
+  });
+  
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
